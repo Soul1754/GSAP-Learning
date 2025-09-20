96d924854df6b6e5fd92bf43b84344fa9ba821a5
s0ulReaPeR <siddhant.gaikwad1754@gmail.com>
2025-09-20 14:10:46 +0530

    bug fixes 1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
     return (
         <nav>
             <div>
-                <a href="#home" className="flex items-center gap-2 ">
+                <a href="#hero" className="flex items-center gap-2 ">
                     <img src="/images/logo.png" alt="" />
                     <p>Soul Tavern</p>
                 </a>
